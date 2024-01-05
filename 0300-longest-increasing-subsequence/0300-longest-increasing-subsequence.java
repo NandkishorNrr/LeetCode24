4f6a0872f251f09ca45220319eb09c8cb3194af0
Nandkishor Rathore <71880979+NandkishorNrr@users.noreply.github.com>
2024-01-05 18:12:20 +0530

    Time: 3 ms (93.88%), Space: 44.4 MB (20.45%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -4,35 +4,27 @@ class Solution {
             return 0;
         }
 
-        int n = nums.length;
-        int[] dp = new int[n];
-        int len = 1;
-        dp[0] = nums[0];
+        int[] tails = new int[nums.length];
+        int len = 0;
 
-        for (int i = 1; i < n; i++) {
-            if (nums[i] > dp[len - 1]) {
-                dp[len] = nums[i];
+        for (int num : nums) {
+            int left = 0, right = len;
+
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (tails[mid] < num) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+
+            tails[left] = num;
+            if (left == len) {
                 len++;
-            } else {
-                int index = binarySearch(dp, 0, len - 1, nums[i]);
-                dp[index] = nums[i];
             }
         }
 
         return len;
-    }
-
-    private static int binarySearch(int[] dp, int left, int right, int target) {
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (dp[mid] == target) {
-                return mid;
-            } else if (dp[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return left;
     }
 }

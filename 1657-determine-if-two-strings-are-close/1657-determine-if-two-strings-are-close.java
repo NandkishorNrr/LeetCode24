c9119d4a3bfa36ffc08c498f41f8b0b25711e884
Nandkishor Rathore <71880979+NandkishorNrr@users.noreply.github.com>
2024-01-14 18:28:53 +0530

    Time: 12 ms (69.52%), Space: 45.6 MB (20.97%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,32 +1,23 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        if (word1.length() != word2.length())
+       if(word1.length() != word2.length())
             return false;
-
         int[] charCount1 = new int[26];
         int[] charCount2 = new int[26];
-        int[] presentInWord1 = new int[26];
-        int[] presentInWord2 = new int[26];
 
-        for (char c : word1.toCharArray()) {
-            charCount1[c - 'a']++;
-            presentInWord1[c - 'a'] = 1;
+        for (int i = 0; i < word1.length(); i++) {
+            charCount1[word1.charAt(i) - 'a']++;
+            charCount2[word2.charAt(i) - 'a']++;
         }
-
-        for (char c : word2.toCharArray()) {
-            charCount2[c - 'a']++;
-            presentInWord2[c - 'a'] = 1;
-        }
-
         for (int i = 0; i < 26; i++) {
-            if ((charCount1[i] == 0 && charCount2[i] != 0) ||
-                (charCount1[i] != 0 && charCount2[i] == 0) ||
-                (presentInWord1[i] == 0 && presentInWord2[i] != 0) ||
-                (presentInWord1[i] != 0 && presentInWord2[i] == 0)) {
+            if ((charCount1[i] == 0 && charCount2[i] != 0) || 
+            (charCount1[i] != 0 && charCount2[i] == 0)) {
                 return false;
             }
         }
+        Arrays.sort(charCount1);
+        Arrays.sort(charCount2);
 
-        return true;
+        return Arrays.equals(charCount1, charCount2);
     }
 }

f510203f1d798c40b4cfab406beacae04fcde0fc
Nandkishor Rathore <71880979+NandkishorNrr@users.noreply.github.com>
2024-01-22 20:50:24 +0530

    Time: 3 ms (81.72%), Space: 45.6 MB (31.9%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,31 +1,24 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         int n = nums.length;
-        
         int[] result = new int[2];
-        
-        for (int i = 0; i < n; i++) {
-            int index = Math.abs(nums[i]) - 1;
-            
-            // If the value at the calculated index is negative, it means we've
-            // seen this index before, indicating the duplicate number.
+
+        for (int num : nums) {
+            int index = Math.abs(num) - 1;
             if (nums[index] < 0) {
-                result[0] = Math.abs(nums[i]);
+                result[0] = Math.abs(num);
             } else {
-                // Mark the value at the calculated index as negative to indicate
-                // that we have seen this index.
                 nums[index] = -nums[index];
             }
         }
-        
-        // Iterate through the array to find the missing number.
+
         for (int i = 0; i < n; i++) {
             if (nums[i] > 0) {
                 result[1] = i + 1;
                 break;
             }
         }
-        
+
         return result;
     }
 }

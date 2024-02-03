18aae923220280c0e9566cb1664a1fed434e632e
Nandkishor Rathore <71880979+NandkishorNrr@users.noreply.github.com>
2024-02-03 20:31:20 +0530

    Time: 71 ms (55.56%), Space: 49.5 MB (8.33%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.js
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.js
@@ -1,14 +1,15 @@
 var maxSumAfterPartitioning = function(arr, k) {
     const n = arr.length;
-    const dp = new Array(n).fill(0);
+    const maxValues = new Array(n).fill(0);
 
     for (let i = 0; i < n; i++) {
         let maxVal = 0;
         for (let j = 1; j <= k && i - j + 1 >= 0; j++) {
             maxVal = Math.max(maxVal, arr[i - j + 1]);
-            dp[i] = Math.max(dp[i], (i >= j ? dp[i - j] : 0) + maxVal * j);
+            const currentSum = (i >= j ? maxValues[i - j] : 0) + maxVal * j;
+            maxValues[i] = Math.max(maxValues[i], currentSum);
         }
     }
 
-    return dp[n - 1];
+    return maxValues[n - 1];
 };

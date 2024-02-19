b3cf819ee8c9171008f3f4f9f4b5a1d060e744a3
Nandkishor Rathore <71880979+NandkishorNrr@users.noreply.github.com>
2024-02-19 07:44:59 +0530

    Time: 78 ms (33.85%), Space: 53.7 MB (8.14%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.js
+++ 0231-power-of-two/0231-power-of-two.js
@@ -3,9 +3,5 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    if (n <= 0) return false; // Negative numbers and zero cannot be powers of two
-    
-    // Use bitwise AND operation to check if there's only one bit set to 1 in the binary representation of n
-    // If n is a power of two, then n & (n - 1) will be 0
-    return (n & (n - 1)) === 0;
+    return n > 0 && (n & (n - 1)) === 0;
 };

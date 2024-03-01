062df4d4e360a26a4f6b83f8e83c297662056b66
Nandkishor Rathore <71880979+NandkishorNrr@users.noreply.github.com>
2024-03-01 09:34:33 +0530

    Time: 82 ms (30.59%), Space: 53.4 MB (14.82%) - LeetHub

---
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.js
@@ -0,0 +1,10 @@
+var maximumOddBinaryNumber = function(s) {
+    let chars = s.split('').sort().reverse();
+    for (let i = chars.length - 1; i >= 0; i--) {
+        if (chars[i] === '1') {
+            [chars[i], chars[chars.length - 1]] = [chars[chars.length - 1], chars[i]];
+            break;
+        }
+    }
+    return chars.join('');
+};

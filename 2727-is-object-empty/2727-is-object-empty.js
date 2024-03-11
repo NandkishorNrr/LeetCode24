9d40d59336a657a1ab89d1b6c3d4275b730d8a4e
Nandkishor Rathore <71880979+NandkishorNrr@users.noreply.github.com>
2024-03-11 20:27:11 +0530

    Time: 56 ms (30.49%), Space: 48.9 MB (72.11%) - LeetHub

---
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -0,0 +1,8 @@
+const isEmpty = obj => {
+    if (Array.isArray(obj)) {
+        return obj.length === 0;
+    } else if (typeof obj === 'object' && obj !== null) {
+        return Object.keys(obj).length === 0;
+    }
+    return false; // If not an array or object, it's not empty
+};

95128f7dbbd320b0a05a3c21531d3ac815a9cac7
Nandkishor Rathore <71880979+NandkishorNrr@users.noreply.github.com>
2024-03-11 20:21:23 +0530

    Time: 53 ms (60.95%), Space: 49.5 MB (42.77%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -0,0 +1,22 @@
+var mergeAlternately = function(word1, word2) {
+    let merged = '';
+    let i = 0;
+    let j = 0;
+    
+    while (i < word1.length && j < word2.length) {
+        merged += word1[i++];
+        merged += word2[j++];
+    }
+    
+    // Append the remaining characters from word1, if any
+    while (i < word1.length) {
+        merged += word1[i++];
+    }
+    
+    // Append the remaining characters from word2, if any
+    while (j < word2.length) {
+        merged += word2[j++];
+    }
+    
+    return merged;
+};

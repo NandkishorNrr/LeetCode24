1245ca88e4413b13f51db9956fa863a565a19536
Nandkishor Rathore <71880979+NandkishorNrr@users.noreply.github.com>
2024-03-14 14:29:23 +0530

    Time: 51 ms (64.65%), Space: 49 MB (37.33%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function(gain) {
+    let maxAltitude = 0;
+    let currentAltitude = 0;
+    
+    for (let i = 0; i < gain.length; i++) {
+        currentAltitude += gain[i]; // Add the gain to the current altitude
+        
+        // Update maxAltitude if currentAltitude is greater
+        if (currentAltitude > maxAltitude) {
+            maxAltitude = currentAltitude;
+        }
+    }
+    
+    return maxAltitude;
+};
